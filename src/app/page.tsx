4a3c7887e69835f869846daa7f23edbc264ebd1e
jhonangelo <jhon@vananz.com>
2023-10-23 09:13:00 +0800

    change font to poppins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
-}
+type HomeProps = {};
+
+const Home = ({}: HomeProps) => {
+  return <div>Home</div>;
+};
+
+export default Home;
