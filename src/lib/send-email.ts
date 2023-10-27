222592ccc7700c14bd9068c91338f315ac1130f2
jhonangelo <jhon@vananz.com>
2023-10-27 19:50:53 +0800

    add email sending functionality

---
+++ src/lib/send-email.ts
@@ -0,0 +1,19 @@
+export function sendEmail(data: {
+  email: string;
+  name: string;
+  message: string;
+}) {
+  const apiEndpoint = '/api/email';
+
+  fetch(apiEndpoint, {
+    method: 'POST',
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      alert(response.message);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
