95847ec1cd974d8867b71a8647f4446e16f1aafc
jhonangelo <jhon@vananz.com>
2023-10-27 22:44:47 +0800

    add conditional rendering for incomplete certificate

--- src/components/Certificate/Certificate.tsx
+++ src/components/Certificate/Certificate.tsx
@@ -14,32 +14,37 @@ const Certificate = (): React.ReactElement => {
   return (
     <section className='max-w-5xl w-screen my-12 px-4 md:px-0 flex-center'>
       <div className='flex-center flex-row flex-wrap mx-10 gap-8 md:gap-10 mt-12'>
-        {certificates.map((item, index) => (
-          <TooltipProvider key={index}>
-            <Tooltip>
-              <TooltipTrigger>
-                <div className='relative w-[75px] md:w-[110px] h-[75px] md:h-[110px]'>
-                  <Link href={item.link}>
-                    <Image
-                      key={index}
-                      src={item.src}
-                      alt={item.title}
-                      fill={true}
-                      className={`${
-                        item.complete &&
-                        'cursor-pointer hover:opacity-100 hover:grayscale-0 hover:scale-105'
-                      } opacity-60 grayscale transition-normal`}
-                    />
-                  </Link>
-                </div>
-              </TooltipTrigger>
-              <TooltipContent className='text-center p-4'>
-                <p>{item.title}</p>
-                <p className='text-xs text-muted-foreground'>Click to visit</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        ))}
+        {certificates.map(
+          (item, index) =>
+            item.complete && (
+              <TooltipProvider key={index}>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <div className='relative w-[75px] md:w-[110px] h-[75px] md:h-[110px]'>
+                      <Link href={item.link}>
+                        <Image
+                          key={index}
+                          src={item.src}
+                          alt={item.title}
+                          fill={true}
+                          className={`${
+                            item.complete &&
+                            'cursor-pointer hover:opacity-100 hover:grayscale-0 hover:scale-105'
+                          } opacity-60 grayscale transition-normal`}
+                        />
+                      </Link>
+                    </div>
+                  </TooltipTrigger>
+                  <TooltipContent className='text-center p-4'>
+                    <p>{item.title}</p>
+                    <p className='text-xs text-muted-foreground'>
+                      Click to visit
+                    </p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            )
+        )}
       </div>
     </section>
   );
