818f5a1bcbe43d5b1f7f0078951a27c5bff73c31
jhonangelo <jhon@vananz.com>
2023-10-27 19:50:14 +0800

    import sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React from 'react';
+import Hero from '@/components/Hero/Hero';
+import About from '@/components/About/About';
+import Projects from '@/components/Projects/Projects';
+import Technology from '@/components/Technology/Technology';
+import Certificate from '@/components/Certificate/Certificate';
+import Contact from '@/components/Contact/Contact';
 
-type HomeProps = {};
-
-const Home = ({}: HomeProps) => {
-  return <main>Home</main>;
+const Home = (): React.ReactElement => {
+  return (
+    <main>
+      <Hero />
+      <About />
+      <Projects />
+      <Technology />
+      <Contact />
+      <Certificate />
+    </main>
+  );
 };
 
 export default Home;
