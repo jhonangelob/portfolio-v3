c0d472bec1ea6c5c68a1108085291a54a72a6c9c
jhonangelo <jhon@vananz.com>
2023-11-02 21:02:57 +0800

    add not-found and error page

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+const Error = (): React.ReactElement => {
+  return (
+    <div className='w-full flex-center h-[calc(100vh-244px)] flex-col mx-auto gap-2'>
+      <h1 className='text-dark text-6xl md:text-9xl font-bold'>404</h1>
+      <p className='text-muted-foreground'>There isn&apos;t anything here.</p>
+    </div>
+  );
+};
+
+export default Error;
