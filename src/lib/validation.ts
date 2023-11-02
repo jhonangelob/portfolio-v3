b26b2da2e88ec201a3c8ca11dbf9faac5b4bc529
jhonangelo <jhon@vananz.com>
2023-11-02 21:03:17 +0800

    rename validations file to validation

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,13 +1,13 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const formSchema = z.object({
   email: z.string().email({
-    message: "Email address must be valid.",
+    message: 'Email address must be valid.',
   }),
   name: z.string().min(3, {
-    message: "Name is required.",
+    message: 'Name is required.',
   }),
   message: z.string().min(2, {
-    message: "Message is required.",
+    message: 'Message is required.',
   }),
 });
