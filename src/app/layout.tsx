31c9fb51e3c86c72b26b58bc6432ac632929c442
jhonangelo <jhon@vananz.com>
2023-11-04 01:25:36 +0800

    fix layout, padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,6 @@ import { Toaster } from '@/components/ui/toaster';
 
 const poppins = Poppins({
   subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins',
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
