c575e951ff8b8efaa34b2f466dc0bd15bdb27cdc
jhonangelo <jhon@vananz.com>
2023-11-04 02:48:15 +0800

    add error constant file

---
+++ src/lib/animate.ts
@@ -0,0 +1,67 @@
+export const animateText = {
+  right: {
+    hidden: { opacity: 0, x: -60 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.75,
+      },
+    },
+    delayChildren: 1,
+  },
+  up: {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  },
+  down: {},
+  left: {},
+  appear: {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.75,
+      },
+    },
+  },
+};
+
+export const animateHeader = {
+  left: {
+    initial: {
+      opacity: 0,
+      x: 120,
+    },
+    animate: {
+      opacity: 1,
+      x: 0,
+      transition: { duration: 0.5 },
+    },
+  },
+  right: {
+    initial: { opacity: 0, x: -120 },
+    animate: {
+      opacity: 1,
+      x: 0,
+      transition: { duration: 0.5 },
+    },
+  },
+};
+
+export const animateFooter = {
+  up: {
+    initial: { opacity: 0, y: 60 },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.5 },
+    },
+  },
+};
