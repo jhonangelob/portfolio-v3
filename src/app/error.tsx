f3a0ab19400314b3d4f8e850b02ba98409764eb5
jhonangelo <jhon@vananz.com>
2023-11-26 18:47:35 +0800

    fix error and not-found page height

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,7 +2,7 @@
 
 const Error = (): React.ReactElement => {
   return (
-    <div className='w-full flex-center h-[calc(100vh-244px)] flex-col mx-auto gap-2'>
+    <div className='w-full flex-center h-[calc(100vh-158px)] flex-col mx-auto gap-2'>
       <h1 className='text-dark text-6xl md:text-9xl font-bold'>500</h1>
       <p className='text-muted-foreground'>Something went wrong.</p>
     </div>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const NotFound = (): React.ReactElement => {
   return (
-    <div className='w-full flex-center h-[calc(100vh-244px)] flex-col mx-auto gap-2'>
+    <div className='w-full flex-center h-[calc(100vh-158px)] flex-col mx-auto gap-2'>
       <h1 className='text-dark text-6xl md:text-9xl font-bold'>404</h1>
       <p className='text-muted-foreground'>There isn&apos;t anything here.</p>
       <Link href='/' className='text-accent-red'>
