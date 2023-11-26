2a8f4acabbf80c8ff1cb589c12db4a2e649c948f
jhonangelo <jhon@vananz.com>
2023-11-27 00:38:07 +0800

    change quote color

--- src/components/Testimonials/TestimonalsCard.tsx
+++ src/components/Testimonials/TestimonalsCard.tsx
@@ -12,12 +12,12 @@ const TestimonalsCard = ({
   return (
     <Card className='w-[350px] bg-transparent py-8 flex flex-col justify-center'>
       <CardContent className='flex flex-row text-sm'>
-        <RxQuote className='rotate-180 w-1/5' />
+        <RxQuote className='rotate-180 w-1/5 text-accent-red' />
         <p className='text-center'>{testimonial.message}</p>
-        <RxQuote className='w-1/5 mt-auto' />
+        <RxQuote className='w-1/5 mt-auto text-accent-red' />
       </CardContent>
       <CardFooter className='flex flex-row gap-2 flex-wrap justify-center text-xs md:text-sm pb-0 text-muted-foreground'>
-        <span className='text-accent-red font-semibold text-xl'>
+        <span className='text-foreground italic font-semibold text-xl'>
           {testimonial.name}
         </span>
         {testimonial.company.position} at {testimonial.company.name}
