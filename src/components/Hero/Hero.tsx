69a1df42d44e6dae32ee6ede0b6e1738b0d58594
jhonangelo <jhon@vananz.com>
2023-11-26 17:24:40 +0800

    fix hero texts

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -15,23 +15,14 @@ const Hero = ({}: HeroProps): React.ReactElement => {
   return (
     <section className='wrapper md:mt-44 md:mb-52'>
       <motion.div
-        className='w-full flex justify-end text-lg md:text-2xl font-semibold u text-muted-foreground italic md:not-italic'
-        ref={ref}
-        initial='hidden'
-        animate={inView ? 'visible' : 'hidden'}
-        variants={animateText.appear}
-      >
-        Software Engineer
-      </motion.div>
-      <motion.div
         className='font-bold text-foreground text-justify md:text-left text-5xl md:text-7xl'
         ref={ref}
         initial='hidden'
         animate={inView ? 'visible' : 'hidden'}
         variants={animateText.up}
       >
-        I specialize in <span className='text-accent-red'>Front-End</span> Web
-        Development and <span className='text-accent-red'>UI/UX</span> Design.
+        I specialize in the intricate domain of{' '}
+        <span className='text-accent-red'>Web Development.</span>
       </motion.div>
     </section>
   );
