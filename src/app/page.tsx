b98352aab695ca4adbd67f4cd114545b58a4726a
jhonangelo <jhon@vananz.com>
2023-11-27 15:23:35 +0800

    remove testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import Projects from '@/components/Projects/Projects';
 import Technology from '@/components/Technology/Technology';
 import Certificate from '@/components/Certificate/Certificate';
 import Contact from '@/components/Contact/Contact';
-import Testimonials from '@/components/Testimonials/Testimonials';
 
 const Home = (): React.ReactElement => {
   return (
@@ -14,7 +13,6 @@ const Home = (): React.ReactElement => {
       <About />
       <Projects />
       <Technology />
-      <Testimonials />
       <Contact />
       <Certificate />
     </main>
--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -2,76 +2,12 @@ import { Projects } from '@/types/projects';
 
 export const projects: Projects = [
   {
-    title: 'Portfolio',
+    title: 'Portfolio v3',
     subtitle: 'You are here',
     description:
       'A website to showcase skills, experiences, and passion within the profession.',
     repository: 'https://github.com/jhonangelob/portfolio-v3',
     link: 'portfolio-v3-delta-opal.vercel.app',
-    technologies: [
-      'next',
-      'typescript',
-      'tailwind',
-      'shadcdn',
-      'sanity',
-      'vercel',
-    ],
-  },
-  {
-    title: 'Portfolio',
-    subtitle: 'You are here',
-    description:
-      'A website to showcase skills, experiences, and passion within the profession.',
-    repository: 'https://github.com/jhonangelob/portfolio-v3',
-    link: '',
-    technologies: [
-      'next',
-      'typescript',
-      'tailwind',
-      'shadcdn',
-      'sanity',
-      'vercel',
-    ],
-  },
-  {
-    title: 'Portfolio',
-    subtitle: 'You are here',
-    description:
-      'A website to showcase skills, experiences, and passion within the profession.',
-    repository: 'https://github.com/jhonangelob/portfolio-v3',
-    link: '',
-    technologies: [
-      'next',
-      'typescript',
-      'tailwind',
-      'shadcdn',
-      'sanity',
-      'vercel',
-    ],
-  },
-  {
-    title: 'Portfolio',
-    subtitle: 'You are here',
-    description:
-      'A website to showcase skills, experiences, and passion within the profession.',
-    repository: 'https://github.com/jhonangelob/portfolio-v3',
-    link: '',
-    technologies: [
-      'next',
-      'typescript',
-      'tailwind',
-      'shadcdn',
-      'sanity',
-      'vercel',
-    ],
-  },
-  {
-    title: 'Portfolio',
-    subtitle: 'You are here',
-    description:
-      'A website to showcase skills, experiences, and passion within the profession.',
-    repository: 'https://github.com/jhonangelob/portfolio-v3',
-    link: '',
     technologies: [
       'next',
       'typescript',
