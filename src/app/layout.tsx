f7a5d6378295374e36e532d3ba75041b23cd331b
jhonangelob <bustarde.aj@gmail.com>
2024-07-07 03:25:06 +0800

    rename

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: 'jhn',
+  title: 'Jhon Angelo Bustarde',
   description: 'Jhon Angelo Bustarde | Software Engineer',
 };
 
